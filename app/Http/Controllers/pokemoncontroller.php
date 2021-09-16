5c099908f9fec791f3b72ccc472bfa67e46f2506
StevenDomingo28 <stevendomingo28@gmail.com>
2021-09-16 17:02:28 +0800

    Initial Commit

---
+++ app/Http/Controllers/pokemoncontroller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+class PokemonController extends Controller
+{   
+    public function getPokemonByName(Request $request) {
+        $client = new Client();
+        $name = strtolower($request->input('pokemon-name'));
+
+        $headers = [
+            'Content-type' => 'application/x-www-form-urlencoded'
+        ];
+        $url = 'https://pokeapi.co/api/v2/pokemon/'.$name;
+        // Send Request to Spotify Auth API
+        $response = $client->request('GET', $url, 
+        [
+            'headers' => $headers
+        ]);
+        
+        $pokemon = json_decode($response->getBody());
+        return $pokemon;
+    }
+}
